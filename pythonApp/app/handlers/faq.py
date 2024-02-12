c67f41134590662ebac04fc3b29649d4fbc00016
peacekeeper228 <sasa.osipov.01@yandex.ru>
2024-02-12 14:58:34 +0300

    tweak faq section

--- pythonApp/app/handlers/faq.py
+++ pythonApp/app/handlers/faq.py
@@ -1,10 +1,10 @@
 from aiogram import Dispatcher, types
 
 async def faq_start(message: types.Message):
-    await message.answer('faq\n\n' \
-                         'Сайты, с которых собираются данные: exchangerate-api.com, cb.rf, metaltorg.ru',
-                         'Список доступных валют можно посмотреть по ссылке: https://www.exchangerate-api.com/docs/supported-currencies \
-                         главный разработчик @Artiom_Nosov')
+    await message.answer('faq\n\n \
+Сайты, с которых собираются данные: exchangerate-api.com, cb.rf, metaltorg.ru \
+Список доступных валют можно посмотреть по ссылке: https://www.exchangerate-api.com/docs/supported-currencies \
+главный разработчик @Artiom_Nosov')
 
 def register_handlers_faq(dp: Dispatcher):
     dp.register_message_handler(faq_start, commands='faq', state='*')
